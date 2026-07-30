4c9ec66aa4ada8042d046073f90c83633fec76ef
Stardust-080925 <ruby35609y@gmail.com>
2026-07-30 21:43:39 +0800

    Add files via upload

---
+++ Migations/20251023022141_AddMessageWithUserRelation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MemberMessageBoard.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMessageWithUserRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Migations/20251104015011_UpdateAllModels.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MemberMessageBoard.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAllModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_Nickname",
+                table: "AspNetUsers",
+                column: "Nickname",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_Nickname",
+                table: "AspNetUsers");
+        }
+    }
+}
